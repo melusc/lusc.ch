e67e09ee4919bbf663af6339d02b1653aa4f2e0c
Luca Schnellmann <oss@lusc.ch>
2026-03-01 08:06:49 +0100

    fix: Correct redirect from gcd-lcm

--- src/routes/gcd-lcm/+page.svelte
+++ src/routes/gcd-lcm/+page.svelte
@@ -3,9 +3,9 @@
 </script>
 
 <svelte:head>
-	<meta http-equiv="refresh" content="0; url=/lcm-gcd/" />
+	<meta http-equiv="refresh" content="0; url=/lcm-gcd" />
 	<title>Redirecting...</title>
 </svelte:head>
 
 <div>Redirecting...</div>
-<div>If that doesn't work click <a href={resolve('/lcm-gcd/')}>here</a></div>
+<div>If that doesn't work click <a href={resolve('/lcm-gcd')}>here</a></div>
